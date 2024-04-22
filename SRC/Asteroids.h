574f0dd9ac991140751e496b052c1058f2754590
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2024-04-22 17:43:37 +0100

    Update Asteroids.h

--- SRC/Asteroids.h
+++ SRC/Asteroids.h
@@ -1,14 +1,19 @@
 #ifndef __ASTEROIDS_H__
 #define __ASTEROIDS_H__
 
-#include "GameUtil.h"
 #include "GameSession.h"
 #include "IKeyboardListener.h"
+#include "GameUtil.h"
 #include "IGameWorldListener.h"
+#include "IKeyboardListener.h"
+#include "IPlayerListener.h"
 #include "IScoreListener.h" 
 #include "ScoreKeeper.h"
 #include "Player.h"
 #include "IPlayerListener.h"
+#include "ScoreKeeper.h"
+#include "Asteroid.h"
+#include <vector>
 
 class GameObject;
 class Spaceship;
@@ -36,7 +41,8 @@ public:
 
 	// Declaration of the IPlayerLister interface //////////////////////////////
 
-	void OnPlayerKilled(int lives_left);
+	void OnPlayerKilled();
+	void OnPowerUpCollected(const string& powerUpType);
 
 	// Declaration of IGameWorldListener interface //////////////////////////////
 
@@ -55,21 +61,38 @@ private:
 	shared_ptr<GUILabel> mTitleLabel;
 	shared_ptr<GUILabel> mStartLabel;
 	shared_ptr<GUILabel> mSelectLabel;
+	shared_ptr<GUILabel> mPowerUpLabel;
+	shared_ptr<GUILabel> mControlsLabel;
+
+	vector<shared_ptr<Asteroid>> mAsteroidList;
 
 	uint mLevel;
 	uint mAsteroidCount;
 	bool mGameStarted;
+	string mSpaceshipSpriteName;
+	bool mIsBulletCooldownActive;
+	bool mIsTimeSlowed;
 
 	shared_ptr<GameObject> CreateSpaceship();
 	void SetSpaceshipSprite(const string& name);
+	void CreateAsteroids(const uint num_asteroids);
+	void CreatePowerUp();
+	void ToggleSlowMotion(bool activate);
+	void TogglePhasing(bool activate);
+	void ToggleBulletCooldown(bool activate);
 	void CreateGUI();
 	void CreateStartScreen();
-	void CreateAsteroids(const uint num_asteroids);
+	void UpdateLivesLabel();
+	void UpdatePowerUpLabel(const string& text, const GLVector3f& colour);
 	shared_ptr<GameObject> CreateExplosion();
 	
 	const static uint SHOW_GAME_OVER = 0;
 	const static uint START_NEXT_LEVEL = 1;
 	const static uint CREATE_NEW_PLAYER = 2;
+	const static uint SPAWN_NEXT_POWERUP = 3;
+	const static uint END_SLOW_MOTION = 4;
+	const static uint END_PHASING = 5;
+	const static uint END_BULLET_COOLDOWN = 6;
 
 	ScoreKeeper mScoreKeeper;
 	Player mPlayer;
